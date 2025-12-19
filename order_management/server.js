3470b25454b30ef498298a440e86bc5da80eabaf
Bidash Gurung <bidashgurung2020@gmail.com>
2025-12-19 15:47:49 +0600

    Orders with adress details

--- order_management/server.js
+++ order_management/server.js
@@ -130,7 +130,7 @@ const server = http.createServer(app);
 
     const PORT = Number(process.env.PORT || 1001);
     server.listen(PORT, "0.0.0.0", () => {
-      console.log(`🚀 Order service + Realtime listening on :${PORT}`);
+      console.log(`🚀 Order service + Realtime listening on port :${PORT}`);
       console.log(`📦 Uploads served at: http://localhost:${PORT}/uploads/...`);
     });
   } catch (err) {
