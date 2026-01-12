3d6ec46c5ec99a5cc1ead5b8efea0b880d6b295b
Bidash Gurung <bidashgurung2020@gmail.com>
2026-01-12 15:23:49 +0600

    Get platormfee changes

--- wallet_payment/models/platformFeeRuleModel.js
+++ wallet_payment/models/platformFeeRuleModel.js
@@ -3,7 +3,8 @@ const db = require("../config/db");
 // Fetch the single fee_percent_bp value
 async function getFeePercentBp() {
   const [rows] = await db.query(
-    "SELECT fee_percent_bp FROM platform_fee_rules LIMIT 1"
+    "SELECT fee_percent_bp FROM platform_fee_rules where service_type=? LIMIT 1",
+    ["Platform Fee"]
   );
   return rows[0] || null;
 }
