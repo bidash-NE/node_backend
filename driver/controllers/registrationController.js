5da225be61da1125ebac69c4f165564ed319884e
Bidash Gurung <bidashgurung2020@gmail.com>
2026-02-07 12:33:30 +0600

    APi for refreshing token

--- driver/controllers/registrationController.js
+++ driver/controllers/registrationController.js
@@ -561,7 +561,7 @@ const verifyActiveSession = async (req, res) => {
     });
 
     const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: "10m",
+      expiresIn: "1440m",
     });
 
     return res.status(200).json({
