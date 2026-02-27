21c155ea7bfda5723e98e1d44ab4151fd6010f50
Bidash Gurung <bidashgurung2020@gmail.com>
2026-02-27 11:48:31 +0600

    Food env changes

--- merchant/server.js
+++ merchant/server.js
@@ -61,5 +61,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, "0.0.0.0", () =>
-  console.log(`🚀 Server running at port NO: ${PORT}`),
+  console.log(`🚀 Server running at port: ${PORT}`),
 );
