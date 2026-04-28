87f261db16ee0588ad3e5bdd21eccd3e5de7503c
Bidash Gurung <bidashgurung2020@gmail.com>
2026-04-28 12:47:34 +0600

    Env file naming changes

--- admin/lib/prisma.js
+++ admin/lib/prisma.js
@@ -3,10 +3,10 @@ const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 const { PrismaClient } = require("../../generated/prisma");
 
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   connectionLimit: 5,
 });
 
